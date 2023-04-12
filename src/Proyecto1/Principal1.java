b67eccea543a07371b1aa714b93998b78760088d
carlosdolmac <carlos.dolmac@educa.jcyl.es>
2023-04-12 18:35:52 +0200

    Nuevo commit

--- src/Proyecto1/Principal1.java
+++ src/Proyecto1/Principal1.java
@@ -3,5 +3,6 @@ package Proyecto1;
 public class Principal1 {
 	public static void main(String[] args) {
 		System.out.println("Hola git número 2");
+		System.out.println("Pruebas Git. Se añade cambio1");
 }
 }
