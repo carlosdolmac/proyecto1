0f2343d47c9a0651283b7cdfb371568f0fbad64a
carlosdolmac <carlos.dolmac@educa.jcyl.es>
2023-04-12 19:09:09 +0200

    Commit conflicto

--- src/Cambio1/Cambio2.java
+++ src/Cambio1/Cambio2.java
@@ -2,10 +2,9 @@ package Cambio1;
 
 public class Cambio2 {
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("Primer proyecto");
-=======
+
 		System.out.println("Clon del proyecto");
->>>>>>> branch 'master' of https://github.com/carlosdolmac/proyecto1.git
 }
 }
